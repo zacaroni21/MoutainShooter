42c12a394f3e3b00d41dc182bd335b87fc8e6bf5
Zacaroni <flavio.zacaroni21@gmail.com>
2025-02-21 13:23:56 -0300

    adding classes generated from StarUML

---
+++ code/backgroud.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Entity import Entity
+from Entity import Entity
+from Entity import Entity
+
+
+class Backgroud(Entity, Entity, Entity):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
---
+++ code/level.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.window = None
+        self.name = None
+        self.entity_list = None
+
+    def run(self, ):
+        pass
